7826ab09f6d33e2e5a8d99b1219ade7543545943
madsfjeldberg <mafj0001@stud.kea.dk>
2025-03-01 17:36:28 +0100

    added .env

--- firebase.js
+++ firebase.js
@@ -1,21 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from "@env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAi1zrcnk1CBZkywdi2Cng3ZdKJImlgwq4",
-  authDomain: "taskminder-94f45.firebaseapp.com",
-  projectId: "taskminder-94f45",
-  storageBucket: "taskminder-94f45.firebasestorage.app",
-  messagingSenderId: "492162730706",
-  appId: "1:492162730706:web:68e62ef81fa340eee73b0b",
-  measurementId: "G-TZJF1BPK3R"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -23,4 +32,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-export { db, collection, addDoc, getDocs };
+export { db };
